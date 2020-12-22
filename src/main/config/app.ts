743025eb86bf60c081bc35f59721a5aabf08ac53
Rafael Coelho <rafael.asilvacoelho@gmail.com>
2020-12-22 15:08:20 -0300

    Add config to go live with graphql server using express middleware

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,11 @@
 import { setupRoutes } from '@/main/config/routes'
+import { setupGraphQlServer } from '@/main/config/graphql-config'
 
 import express from 'express'
 
 const app = express()
+
 setupRoutes(app)
+setupGraphQlServer(app)
 
 export default app
---
+++ src/main/config/graphql-config.ts
@@ -0,0 +1,14 @@
+import typeDefs from '@/main/graphql/type-defs'
+import resolvers from '@/main/graphql/resolvers'
+
+import { ApolloServer } from "apollo-server-express"
+import { Express } from 'express'
+
+export const setupGraphQlServer = (app: Express): void => {
+    const server = new ApolloServer({
+        resolvers,
+        typeDefs
+    })
+
+    server.applyMiddleware({ app })
+}
